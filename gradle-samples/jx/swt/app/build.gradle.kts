eacdb8b0047f7eb966fe334031d9bacd0e68cfb4
mauro <mauropuravida@gmail.com>
2024-01-04 09:42:21 -0300

    added swing and jx samples
    [ci skip]

---
+++ gradle-samples/jx/swt/app/build.gradle.kts
@@ -0,0 +1,53 @@
+
+val chromiumVersion = "106.0.22"
+val chromiumPlatformVersion = "106.0.20"
+val chromiumJxVersion = "106.0.0.0"
+var vmArgs = "-Dempty"
+var envVars = mutableMapOf<String, String>()
+val os = System.getProperty("os.name").toLowerCase()
+val platform = when {
+    os.contains("linux") -> {
+    	envVars["GDK_BACKEND"] = "x11"
+        vmArgs = "-Dchromium.init_threads=true"
+        "gtk.linux"
+    }
+    os.contains("win") -> "win32.win32"
+    os.contains("mac") -> {
+        vmArgs = "-XstartOnFirstThread"
+        "cocoa.macosx"
+    }
+    else -> ""
+}
+
+configurations.all {
+    resolutionStrategy.eachDependency {
+        if (requested.name.contains("org.eclipse.swt.")) {
+            useTarget("${requested.group}:org.eclipse.swt.${platform}.x86_64:${requested.version}")
+        }
+    }
+}
+
+plugins {
+    kotlin("jvm") version "1.5.10"
+    application
+}
+
+repositories {
+    mavenCentral()
+    maven(url = "https://dl.equo.dev/chromium-swt-ee/equoSamples/mvn")
+}
+
+dependencies {
+    implementation("com.equo:com.equo.chromium.cef.${platform}.x86_64:${chromiumPlatformVersion}")
+    implementation("com.equo:com.equo.chromium.jx:${chromiumJxVersion}")
+    implementation("org.eclipse.platform:org.eclipse.swt.${platform}.x86_64:3.121.0")
+    implementation("org.eclipse.platform:org.eclipse.swt:3.121.0")
+}
+
+application {
+    applicationDefaultJvmArgs = listOf("${vmArgs}")
+    mainClass.set("SampleSWT.SampleSWTKt")
+    tasks.named<JavaExec>("run") {
+        environment(envVars)
+    }
+}
