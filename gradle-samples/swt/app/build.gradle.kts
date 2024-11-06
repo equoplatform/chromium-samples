94c0b02881fced6cf94e7b193144ad995dc865a8
“facundoignacio9” <facundoignacio9@gmail.com>
2024-11-06 11:51:21 -0300

    use correct patch number

--- gradle-samples/swt/app/build.gradle.kts
+++ gradle-samples/swt/app/build.gradle.kts
@@ -1,8 +1,8 @@
 
 var platform = ""
 var vmArgs = mutableListOf<String>()
-val chromiumVersion = "128.0.0"
-val chromiumPlatformVersion = "128.0.0"
+val chromiumVersion = "128.0.1"
+val chromiumPlatformVersion = "128.0.1"
 val os = System.getProperty("os.name").toLowerCase()
 if (os.contains("linux")) {
     platform = "gtk.linux"
